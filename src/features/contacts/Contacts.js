998c3be91ae88d4c45fcd2e0ce2c346e1fbb644f
YuliiaMoholivets <veronikaanddiana1@gmail.com>
2023-02-22 21:03:15 +0200

    Update Contacts.js

--- src/features/contacts/Contacts.js
+++ src/features/contacts/Contacts.js
@@ -2,7 +2,6 @@ import { CreateContact } from "./CreateContact";
 import { ContactsFilter } from './ContactsFilter';
 import { ContactsList } from './ContactsList';
 import { useSelector } from "react-redux";
-// import styles from "./Contacts.module.css";
 import { contactsSelect} from 'redux/selector';
 
 export const ContactPage = () => {
